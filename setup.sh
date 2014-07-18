b7b37aa95eff183c76af2841a12d1bd23442e45b
Josh Harvey <josh@lamassu.is>
2014-07-18 12:35:18 -0400

    updated setup file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+mkdir -p data/log
+mkdir -p ui/css/fonts
+mkdir /tmp/lamassu-fonts
+curl -L http://sourceforge.net/projects/sourcesans.adobe/files/latest/download > /tmp/lamassu-fonts/source-pro.zip
+unzip /tmp/source-pro.zip -d /tmp/lamassu-fonts
+cp /tmp/lamassu-fonts/source-sans-pro-*/TTF/*.ttf ui/css/fonts
+rm -rf /tmp/lamassu-fonts
 
-mkdir -p /usr/local/share/sencha/config
-mkdir -p /var/lib/sencha/config
-mkdir -p /var/lib/sencha/log
-cp device_config.json /usr/local/share/sencha/config
-cp unit_config.json /usr/local/share/sencha/config
-cp user_config.json /var/lib/sencha/config
