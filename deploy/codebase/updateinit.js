2650832d133fd6abaee50781138ea73e304cffce
Josh Harvey <josh@lamassu.is>
2014-08-12 18:12:01 -0400

    fix(deploy) don't try to remount as read-only filesystem

--- deploy/codebase/updateinit.js
+++ deploy/codebase/updateinit.js
@@ -16,12 +16,7 @@ function command(cmd, cb) {
 
 function remountRW(cb) {
   if (hardwareCode !== 'N7G1') return cb();
-  command('mount -o remount,rw /dev/root', cb);
-}
-
-function remountRO(cb) {
-  if (hardwareCode !== 'N7G1') return cb();
-  command('mount -o remount,ro /dev/root', cb);
+  command('/bin/mount -o remount,rw /', cb);
 }
 
 async.series([
@@ -30,7 +25,6 @@ async.series([
   async.apply(command, 'cp -a /tmp/extract/package/lamassu-machine /opt/apps/machine'),
   async.apply(command, 'cp -a /tmp/extract/package/hardware/' + hardwareCode + '/node_modules /opt/apps/machine/lamassu-machine'),
   async.apply(command, 'cp /tmp/extract/package/hardware/' + hardwareCode + '/device_config.json /opt/apps/machine/lamassu-machine'),
-  async.apply(remountRO),
   async.apply(report, null, 'finished.')
 ], function(err) {
   if (err) throw err;
