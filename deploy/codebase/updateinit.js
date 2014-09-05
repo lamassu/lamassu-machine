9bd0cf5d8cabe33ea9e63c45ddca63d068a97576
Josh Harvey <josh@lamassu.is>
2014-09-05 17:10:00 -0400

    feat(build) build zipped subpackage

--- deploy/codebase/updateinit.js
+++ deploy/codebase/updateinit.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var fs = require('fs');
+var zlib = require('zlib');
 var async = require('./async');
 var cp = require('child_process');
 var report = require('./report').report;
+var tar = require('/opt/apps/machine/lamassu-machine/node_modules/tar');
 
 var TIMEOUT = 10000;
 
@@ -19,13 +22,21 @@ function remountRW(cb) {
   command('/bin/mount -o remount,rw /', cb);
 }
 
+function untar(tarball, outPath, cb) {
+  var fileIn = fs.createReadStream(tarball);
+  fileIn.pipe(zlib.createDeflate()).pipe(tar.Extract(outPath))
+  .on('error', cb)
+  .on('end', cb);   // success
+}
+
 async.series([
   async.apply(remountRW),
   async.apply(command, 'mkdir -p /opt/apps/machine'),
-  async.apply(command, 'cp -a /tmp/extract/package/lamassu-machine /opt/apps/machine'),
-  async.apply(command, 'cp -a /tmp/extract/package/hardware/' + hardwareCode + '/node_modules /opt/apps/machine/lamassu-machine'),
-  async.apply(command, 'cp /tmp/extract/package/hardware/' + hardwareCode + '/device_config.json /opt/apps/machine/lamassu-machine'),
-  async.apply(command, 'cp /tmp/extract/package/hardware/' + hardwareCode + '/lib/* /usr/local/lib'),
+  async.apply(untar, '/tmp/extract/package/subpackage.tgz', '/tmp/extract/package/'),
+  async.apply(command, 'cp -a /tmp/extract/package/subpackage/lamassu-machine /opt/apps/machine'),
+  async.apply(command, 'cp -a /tmp/extract/package/subpackage/hardware/' + hardwareCode + '/node_modules /opt/apps/machine/lamassu-machine'),
+  async.apply(command, 'cp /tmp/extract/package/subpackage/hardware/' + hardwareCode + '/device_config.json /opt/apps/machine/lamassu-machine'),
+  async.apply(command, 'cp /tmp/extract/package/subpackage/hardware/' + hardwareCode + '/lib/* /usr/local/lib'),
   async.apply(report, null, 'finished.')
 ], function(err) {
   if (err) throw err;
