87a19fa204b687428eddb484a3c5a3fc27e658df
Josh Harvey <josh@lamassu.is>
2014-11-27 00:48:01 -0500

    separate first name from middle name

--- bin/parse_pdf.js
+++ bin/parse_pdf.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var util = require('util');
 var parser = require('../lib/compliance/parsepdf417');
 
-var licensePath = 'mock_data/compliance/fl.dat';
-//var licensePath = 'mock_data/compliance/nh.dat';
+//var licensePath = 'mock_data/compliance/fl.dat';
+var licensePath = 'mock_data/compliance/nh.dat';
 var data = fs.readFileSync(licensePath, 'utf8');
 var result = parser.parse(data);
 console.log(util.inspect(result, {depth: null, colors: true}));
