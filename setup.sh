1f114100c8e6bb17b5ec9f8ba6dfb0c9e7642fe6
Johnathan <haxwell@gmail.com>
2014-12-06 13:49:31 -0700

    lamassu/lamassu-machine issue #30, updating setup.sh with new urls for fonts, and removing calls to deploy/fonts/install

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 mkdir -p data/log
 mkdir -p ui/css/fonts
-mkdir /tmp/lamassu-fonts
+mkdir -p /tmp/lamassu-fonts
+rm /tmp/lamassu-fonts/* -rf
 echo "Downloading fonts..."
-curl -# -L http://sourceforge.net/projects/sourcesans.adobe/files/latest/download > /tmp/lamassu-fonts/source-sans-pro.zip
-curl -# -L http://sourceforge.net/projects/sourcecodepro.adobe/files/SourceCodePro_FontsOnly-1.017.zip/download > /tmp/lamassu-fonts/source-code-pro.zip
+curl -# -L https://github.com/adobe-fonts/source-sans-pro/archive/2.010R-ro/1.065R-it.zip > /tmp/lamassu-fonts/source-sans-pro.zip
+curl -# -L https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip > /tmp/lamassu-fonts/source-code-pro.zip
+echo "Installing fonts in lamassu-machine..."
 unzip -q /tmp/lamassu-fonts/source-sans-pro.zip -d /tmp/lamassu-fonts
 unzip -q /tmp/lamassu-fonts/source-code-pro.zip -d /tmp/lamassu-fonts
-deploy/fonts/install source-sans /tmp/lamassu-fonts/source-sans-pro-*/TTF .
-deploy/fonts/install source-code-pro /tmp/lamassu-fonts/source-code-pro-*/TTF .
+cp -rf /tmp/lamassu-fonts/source-sans-pro-*/TTF ui/css/fonts
+cp -rf /tmp/lamassu-fonts/source-code-pro-*/TTF ui/css/fonts
+echo "Setting up config files..."
 cp device_config.sample.json device_config.json
+echo "Setting up keys..."
 cp data/client.sample.key data/client.key
 cp data/client.sample.pem data/client.pem
 echo "Successful installation."
