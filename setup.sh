a0a3eab91d9c9957dc5cda7e41112fa42b764422
Johnathan <haxwell@gmail.com>
2014-12-06 17:05:29 -0700

    Now runs 'npm install' if the node_modules directory is not present. Also, tells user to configure licenses.json if that file is not present.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,16 @@ mkdir -p data/log
 mkdir -p ui/css/fonts
 mkdir -p /tmp/lamassu-fonts
 rm /tmp/lamassu-fonts/* -rf
+
+echo
+
+if [ ! -d "node_modules" ]; then
+    echo "node.js dependencies not yet installed.. installing them now."
+    npm install
+else
+    echo "node_modules folder found. skipping dependency install."
+fi
+
 echo "Downloading fonts..."
 curl -# -L https://github.com/adobe-fonts/source-sans-pro/archive/2.010R-ro/1.065R-it.zip > /tmp/lamassu-fonts/source-sans-pro.zip
 curl -# -L https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip > /tmp/lamassu-fonts/source-code-pro.zip
@@ -16,4 +26,11 @@ cp device_config.sample.json device_config.json
 echo "Setting up keys..."
 cp data/client.sample.key data/client.key
 cp data/client.sample.pem data/client.pem
+
+if [ ! -e "licenses.json" ]; then
+    echo
+    echo "licenses.json not found. Edit the licenses.sample.json file, and add your API keys manually."
+    echo 
+fi
+
 echo "Successful installation."
