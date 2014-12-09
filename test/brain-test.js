cbce2a6c30b17c2e7e67b7b560b0b8b4d93a62bd
Johnathan <haxwell@gmail.com>
2014-12-08 21:19:53 -0700

    Refactored the logic dealing with configuration into its own module so that it could be accessed by the unit test brain-test.js

--- test/brain-test.js
+++ test/brain-test.js
@@ -1,8 +1,20 @@
 'use strict';
 
+var Configuration = require('../lib/configuration.js');
+var Brain = require("../lib/brain");
+
 // sample test spec
 describe("A suite", function() {
+	var config = null;
+	
+	beforeAll(function () {
+	  var commandLine = JSON.parse('{"_":[],"mockBTC":"1EyE2nE4hf8JVjV51Veznz9t9vTFv8uRU5","mockBv":"/dev/pts/7","mockTrader":true,"mockCam":true,"mockBillDispenser":true}');
+	  config = Configuration.loadConfig(commandLine);
+	});
+	
   it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+	  var brain = new Brain(config);
+	  
+	  expect(true).toBe(true);
   });
 });
