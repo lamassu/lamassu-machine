553652212812808de6fcdd8293e786e79cbc02f2
Johnathan <haxwell@gmail.com>
2014-12-09 20:23:53 -0700

    Check if the jasmine and istanbul symbolic links are present before attempting to create them.

--- setup.sh
+++ setup.sh
@@ -9,8 +9,12 @@ echo
 if [ ! -d "node_modules" ]; then
     echo "node.js dependencies not yet installed.. installing them now."
     npm install
-    ln -s ./node_modules/jasmine/bin/jasmine.js jasmine
-    ln -s ./node_modules/istanbul/lib/cli.js istanbul
+    if [ ! -e "jasmine" ]; then 
+        ln -s ./node_modules/jasmine/bin/jasmine.js jasmine 
+    fi
+    if [ ! -e "istanbul" ]; then 
+        ln -s ./node_modules/istanbul/lib/cli.js istanbul 
+    fi
 else
     echo "node_modules folder found. skipping dependency install."
 fi
