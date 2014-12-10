754b0cee426b17c0af860021fc9a5264835d144d
Johnathan <haxwell@gmail.com>
2014-12-10 08:26:55 -0700

    Added test to verify trader event listeners are set up correctly

--- test/brain-test.js
+++ test/brain-test.js
@@ -1,24 +1,34 @@
 'use strict';
 
-var Configuration = require("../lib/configuration.js");
-var Brain = require("../lib/brain");
-var State = require("../lib/constants/state.js");
+var _ = require('lodash');
+var Brain = require('../lib/brain');
+var Configuration = require('../lib/configuration.js');
+var State = require('../lib/constants/state.js');
 
-describe("Brain", function() {
+describe('Brain', function() {
 	var config = null;
+	var brain = null;
 	
-	beforeAll(function () {
-	  var commandLine = JSON.parse('{"_":[],"mockBTC":"1EyE2nE4hf8JVjV51Veznz9t9vTFv8uRU5","mockBv":"/dev/pts/7","mockTrader":true,"mockCam":true,"mockBillDispenser":true}');
+	beforeEach(function () {
+	  var commandLine = JSON.parse("{'_':[],'mockBTC':'1EyE2nE4hf8JVjV51Veznz9t9vTFv8uRU5','mockBv':'/dev/pts/7','mockTrader':true,'mockCam':true,'mockBillDispenser':true}");
 	  config = Configuration.loadConfig(commandLine);
+	  
+	  brain = new Brain(config);
 	});
 	
-  it("can be configured with default values", function() {
-	  var brain = new Brain(config);
+  it('can be configured with default values', function() {
 	  expect(brain).toBeDefined();
   });
   
-  it("starts off in the state 'start'", function () {
-	  var brain = new Brain(config);
+  it('starts off in the state \'start\'', function () {
 	  expect(brain.state).toBe(State.START);
+  });
+
+  it('initializes its trader correctly', function () {
+	  var expectedTraderEvents = [State.POLL_UPDATE, 'networkDown', 'networkUp', 'dispenseUpdate', 'error', 'unpair'];
+	  
+	  brain._initTraderEvents();
+	  
+	  _.each(expectedTraderEvents, function(el/*, idx, list*/) { var arr = brain.trader.listeners(el); expect(arr.length).toBe(1); });
   });
 });
