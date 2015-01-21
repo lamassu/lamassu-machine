172ecfdd9487ef045734486ba3a5afa11569a6c5
Josh Harvey <josh@lamassu.is>
2015-01-21 17:15:50 -0500

    add SourceSansPro fonts in build

--- deploy/codebase/build.sh
+++ deploy/codebase/build.sh
@@ -51,6 +51,7 @@ rm -rf $TARGET_MACHINE_DIR/ui/css/fonts/*
 cp $MACHINE_DIR/ui/css/fonts/brandon_txt* $TARGET_MACHINE_DIR/ui/css/fonts
 cp $MACHINE_DIR/ui/css/fonts/SourceCodePro-Regular.ttf $TARGET_MACHINE_DIR/ui/css/fonts
 cp $MACHINE_DIR/ui/css/fonts/NotoKufiArabic* $TARGET_MACHINE_DIR/ui/css/fonts
+cp -a $MACHINE_DIR/ui/css/fonts/SourceSansPro $TARGET_MACHINE_DIR/ui/css/fonts
 
 # Natively compiled modules, will be copied from hardware-specific directories
 rm -rf $TARGET_MODULES_DIR/ws
