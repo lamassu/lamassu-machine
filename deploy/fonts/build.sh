735b05e28a33bed0f5f972b89852f96ff014a033
Josh Harvey <josh@lamassu.is>
2015-01-22 01:14:38 -0500

    tweak font deploy

--- deploy/fonts/build.sh
+++ deploy/fonts/build.sh
@@ -7,7 +7,7 @@ if [ -z "$FONT_DIR" ]
   then
     echo "Builds a lamassu-machine package file for installing a supplemental font."
     echo -e "\nUsage:"
-    echo -e "build <supplemental font directory in css/fonts>\n"
+    echo -e "build <supplemental font directory in fonts>\n"
     exit 1
 fi
 
@@ -16,18 +16,15 @@ SCRIPT_DIR=$(dirname $0)
 MACHINE_DIR=$SCRIPT_DIR/../..
 EXPORT_ROOT=$MACHINE_DIR/build
 
-EXPORT_BASE=$EXPORT_ROOT/$SUB_DIR
+EXPORT_BASE=$EXPORT_ROOT/$SUB_DIR/$FONT_DIR
 EXPORT_DIR=$EXPORT_BASE/subpackage
 EXPORT_SCRIPT_DIR=$EXPORT_BASE/package
-TARGET_MACHINE_DIR=$EXPORT_DIR/lamassu-machine
 HARDWARE_DIR=$MACHINE_DIR/hardware/codebase
 UPDATESCRIPT=$SCRIPT_DIR/updateinit.js
-TARGET_MODULES_DIR=$TARGET_MACHINE_DIR/node_modules
 rm -rf $EXPORT_SCRIPT_DIR
 rm -rf $EXPORT_DIR
 mkdir -p $EXPORT_DIR
 mkdir -p $EXPORT_SCRIPT_DIR
-mkdir -p $TARGET_MACHINE_DIR
 
 # Needed for updateinit script on target device
 cp $MACHINE_DIR/node_modules/async/lib/async.js $EXPORT_SCRIPT_DIR
@@ -36,7 +33,7 @@ cp -a $MACHINE_DIR/node_modules/fs-extra $EXPORT_SCRIPT_DIR/node_modules
 cp $SCRIPT_DIR/../report.js $EXPORT_SCRIPT_DIR
 cp $UPDATESCRIPT $EXPORT_SCRIPT_DIR/updatescript.js
 
-cp -a $MACHINE_DIR/ui/css/fonts/$FONT_DIR $EXPORT_DIR
+cp -a $MACHINE_DIR/fonts/$FONT_DIR $EXPORT_DIR
 
 node $SCRIPT_DIR/../build.js $EXPORT_BASE
 
