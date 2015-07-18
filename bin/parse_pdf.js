e024bf40e0df9875c2e792aa03c69d6c41a59a11
Josh Harvey <josh@lamassu.is>
2015-07-18 22:20:50 +0200

    fix for NC license

--- bin/parse_pdf.js
+++ bin/parse_pdf.js
@@ -6,6 +6,7 @@ var parser = require('../lib/compliance/parsepdf417')
 
 // var licensePath = 'mock_data/compliance/fl.dat'
 var licensePath = 'mock_data/compliance/nh.dat'
+// var licensePath = 'scratch/nc2.dat'
 var data = fs.readFileSync(licensePath, 'utf8')
 var result = parser.parse(data)
 console.log(util.inspect(result, {depth: null, colors: true}))
