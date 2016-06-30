00216d0d5ec603957690efc60562d0678583e0c4
Josh Harvey <josh@lamassu.is>
2016-06-30 15:40:38 +0100

    fix \r bug in PDF417 parsing

--- bin/parse_pdf.js
+++ bin/parse_pdf.js
@@ -5,8 +5,10 @@ var util = require('util')
 var parser = require('../lib/compliance/parsepdf417')
 
 // var licensePath = 'mock_data/compliance/fl.dat'
-var licensePath = 'mock_data/compliance/nh.dat'
+var licensePath = 'mock_data/compliance/ny.dat'
 // var licensePath = 'scratch/nc2.dat'
 var data = fs.readFileSync(licensePath, 'utf8')
+data = data.replace('&', '\r')
+console.log(data)
 var result = parser.parse(data)
 console.log(util.inspect(result, {depth: null, colors: true}))
