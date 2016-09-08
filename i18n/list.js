8dd77e81f54a42e6ac678dc1396dcafb1ffd1096
Josh Harvey <josh@lamassu.is>
2016-09-08 15:16:55 +0300

    fix i18n/list.js

--- i18n/list.js
+++ i18n/list.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 var path = require('path');
 
 var localePath = path.resolve(__dirname, '..', 'ui', 'js', 'locales.js');
-var json = fs.readFileSync(localePath).slice(14, -1).toString();
+var json = fs.readFileSync(localePath).slice(14, -2).toString();
+
 var results = JSON.parse(json);
 
 Object.keys(results).sort().forEach(function (locale) {
