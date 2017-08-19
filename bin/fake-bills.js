93121c8f35a0bfc2f82989aceb6ae87c5e3547d1
Josh Harvey <josh@lamassu.is>
2017-08-20 02:18:59 +0300

    add billValidator dev script

--- bin/fake-bills.js
+++ bin/fake-bills.js
@@ -2,17 +2,29 @@ const net = require('net')
 const readline = require('readline')
 const _ = require('lodash/fp')
 
-const server = net.createServer((socket) => {
-  getInput()
-  socket.end('goodbye\n')
-}).on('error', (err) => {
-  console.log(err)
-})
+let client
 
-server.listen({port: 3077}, (err, res) => {
-  if (err) throw err
-  console.log('Bills server listening on port 3077')
-})
+function _connect () {
+  if (client && !client.destroyed) return
+
+  client = net.connect({port: 3077}, () => {
+    console.log('connected to lamassu-machine!')
+    getInput(client)
+  })
+
+  client.on('end', () => {
+    console.log('disconnected from server')
+  })
+
+  client.on('error', _ => {})
+}
+
+function connect () {
+  try {
+    _connect()
+  } catch (_) {
+  }
+}
 
 function safeParseInt (s) {
   try {
@@ -22,20 +34,20 @@ function safeParseInt (s) {
   }
 }
 
-function getInput (socket) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: 'bill> '
-  })
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'bill> '
+})
 
+function getInput (client) {
   rl.prompt()
 
   rl.on('line', line => {
     const denomination = safeParseInt(line.trim())
 
     if (_.isInteger(denomination)) {
-      socket.write(JSON.stringify({command: 'insertBill', denomination}))
+      client.write(JSON.stringify({command: 'insertBill', denomination}))
     } else {
       console.log('Please enter an integer.\n')
     }
@@ -46,3 +58,7 @@ function getInput (socket) {
     process.exit(0)
   })
 }
+
+console.log('Connecting to lamassu-machine...')
+connect()
+setInterval(connect, 1000)
