1e4cf59a17119ba249826c81410ff27cb9c5dcdf
Josh Harvey <josh@lamassu.is>
2017-08-28 11:44:09 +0300

    Add tx versioning

--- ui/js/app.js
+++ ui/js/app.js
@@ -155,7 +155,6 @@ function processData(data) {
       setState('security_code');
       break;
     case 'scanned':
-      confirmBeep.play();
       setState('insert_bills');
       break;
     case 'acceptingFirstBill':
