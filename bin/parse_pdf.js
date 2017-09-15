3ac1b65688c8a3dfa8a7e96f65659f7067e0f579
Josh Harvey <josh@lamassu.is>
2017-09-15 19:36:59 +0300

    fix pdf417 for colorado

--- bin/parse_pdf.js
+++ bin/parse_pdf.js
@@ -1,12 +1,13 @@
 'use strict'
 
 var fs = require('fs')
+var path = require('path')
 var util = require('util')
 var parser = require('../lib/compliance/parsepdf417')
 
-// var licensePath = 'mock_data/compliance/fl.dat'
-var licensePath = 'mock_data/compliance/nh.dat'
-// var licensePath = 'scratch/nc2.dat'
+var licensePath = process.argv[2]
+console.log(path.resolve(__dirname, '..', licensePath))
+
 var data = fs.readFileSync(licensePath, 'utf8')
 var result = parser.parse(data)
 console.log(util.inspect(result, {depth: null, colors: true}))
