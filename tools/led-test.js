a312517f745a98c116d76a5a78afd785e16545ce
Josh Harvey <josh@lamassu.is>
2018-02-11 00:30:57 +0100

    WIPP

--- tools/led-test.js
+++ tools/led-test.js
@@ -4,3 +4,11 @@ const COLORS = ledControl.COLORS
 const LEDS = ledControl.LEDS
 
 ledControl.lightUp({type: 'solid', color: COLORS.red, range: LEDS.doorLeds})
+
+setTimeout(() => ledControl.lightDown(), 1000)
+
+setTimeout(() => ledControl.lightUp({type: 'pulse', color: COLORS.amazonite, range: LEDS.doorLeds}), 500)
+
+setTimeout(() => ledControl.lightDown(), 2000)
+
+process.on('unhandledRejection', console.log)
