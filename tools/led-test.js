a6cfffe6e8a08e838e42f46057b2451e19c4c7f4
Josh Harvey <josh@lamassu.is>
2018-02-15 23:17:31 +0200

    WIP

--- tools/led-test.js
+++ tools/led-test.js
@@ -3,12 +3,6 @@ const ledControl = require('../lib/ssuboard/led-control')
 const COLORS = ledControl.COLORS
 const LEDS = ledControl.LEDS
 
-ledControl.lightUp({type: 'solid', color: COLORS.red, range: LEDS.doorLeds})
-
-setTimeout(() => ledControl.lightDown(), 1000)
-
-setTimeout(() => ledControl.lightUp({type: 'pulse', color: COLORS.amazonite, range: LEDS.doorLeds}), 500)
-
-setTimeout(() => ledControl.lightDown(), 2000)
+ledControl.lightUp({type: 'solid', color: COLORS.orange, range: LEDS.doorLeds})
 
 process.on('unhandledRejection', console.log)
