eca78753013c853b985d883f17d52cac07c1a091
Josh Harvey <josh@lamassu.is>
2018-02-15 15:47:22 +0200

    WIP

--- tools/door-test.js
+++ tools/door-test.js
@@ -4,20 +4,23 @@ const actionEmitter = require('../lib/action-emitter')
 
 const doorManager = require('../lib/ssuboard/door-manager')
 const u2f = require('../lib/ssuboard/u2f')
+const io = require('../lib/ssuboard/io')
 
-u2f.run()
-.then(doorManager.run)
+doorManager.run()
+.then(u2f.run)
+.then(io.run)
 .then(() => console.log('All set up.'))
 
-actionEmitter.on('fob', console.log)
+actionEmitter.on('fob', r => console.log('DEBUG101: %j', r))
 
 actionEmitter.on('doorManager', r => {
+  console.log('DEBUG100')
   console.log(r)
 
   if (r.action === 'popDoor') {
     console.log('*** Door popped ***')
-    setTimeout(() => actionEmitter.emit('door', {action: 'doorNotSecured'}), 500)
-    setTimeout(() => actionEmitter.emit('door', {action: 'doorSecured'}), 2000)
+    // setTimeout(() => actionEmitter.emit('door', {action: 'doorNotSecured'}), 500)
+    // setTimeout(() => actionEmitter.emit('door', {action: 'doorSecured'}), 2000)
   }
 })
 
