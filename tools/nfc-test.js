1bb45442acfceae21d13522112f755bfd1761e2e
Josh Harvey <josh@lamassu.is>
2018-03-06 18:03:35 +0000

    tweaks

--- tools/nfc-test.js
+++ tools/nfc-test.js
@@ -2,8 +2,6 @@ const process = require('process')
 
 const actionEmitter = require('../lib/action-emitter')
 
-const u2f = require('../lib/ssuboard/u2f')
-
 const nfc = require('../lib/ssuboard/nfc')
 
 const _ = require('lodash/fp')
@@ -19,7 +17,7 @@ function delay () {
   return new Promise(resolve => setTimeout(resolve, 1000))
 }
 
-nfc.run()
+nfc.run('ACS ACR1281 1S PICC Reader(1)')
 .catch(console.log)
 
 process.on('unhandledRejection', e => console.log(e.trace))
