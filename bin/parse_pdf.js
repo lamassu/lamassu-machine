b45ed680f3500114c85ae322b323c013b783088e
Josh Harvey <josh@lamassu.is>
2018-05-17 10:23:38 +0300

    support MN enhanced license

--- bin/parse_pdf.js
+++ bin/parse_pdf.js
@@ -5,8 +5,8 @@ var util = require('util')
 var parser = require('../lib/compliance/parsepdf417')
 
 // var licensePath = 'mock_data/compliance/fl.dat'
-var licensePath = 'mock_data/compliance/ny.dat'
-// var licensePath = 'scratch/nc2.dat'
+// var licensePath = 'mock_data/compliance/ny.dat'
+var licensePath = 'scratch/mn.dat'
 var data = fs.readFileSync(licensePath, 'utf8')
 data = data.replace('&', '\r')
 console.log(data)
