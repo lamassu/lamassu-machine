001a66dab1839f1d0236219d3f55b793a004ff8e
Josh Harvey <josh@lamassu.is>
2018-05-22 10:52:31 +0300

    add uid to all license types

--- bin/parse_pdf.js
+++ bin/parse_pdf.js
@@ -4,14 +4,13 @@ var fs = require('fs')
 var util = require('util')
 var parser = require('../lib/compliance/parsepdf417')
 
-// var licensePath = 'mock_data/compliance/fl.dat'
+var licensePath = 'mock_data/compliance/fl.dat'
 // var licensePath = 'mock_data/compliance/ny.dat'
-var licensePath = 'scratch/mn.dat'
+// var licensePath = 'scratch/mn.dat'
 
 var data = fs.readFileSync(licensePath, 'utf8')
 data = data.replace('&', '\r')
 
-// const data = 'SIMPSON|MICHAEL|M|RSA|6512395184073|28 NOV 1976|RSA|CITIZEN|02 OCT 2018|87555|115641856|123456789112345678901234567890123456789013345678901234567894123456789012345678901234567890123456789012345678'
 console.log(data)
 var result = parser.parse(Buffer.from(data))
 console.log(util.inspect(result, {depth: null, colors: true}))
