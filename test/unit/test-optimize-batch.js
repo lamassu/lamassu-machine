3f6b5976abea81ca0dea08862b6b6459a3e7541e
Zoran Joka <zoran.joka@gmail.com>
2018-09-25 13:04:32 +0200

    Implement requested changes, add unit test

---
+++ test/unit/test-optimize-batch.js
@@ -0,0 +1,37 @@
+import _ from 'lodash/fp'
+import test from 'ava'
+import optimizeDispense from '../../lib/dispense-optimizer'
+
+const LIMIT = 20
+
+const optimizeDispenseWithLimit = inputArr => optimizeDispense(inputArr, LIMIT)
+
+test('Pass only one batch', t => {
+  const o = optimizeDispenseWithLimit([2, 6])
+
+  t.is(o.length, 1)
+})
+
+test('Pass multiple batches where a < b', t => {
+  const o = optimizeDispenseWithLimit([4, 43])
+
+  t.is(o.length, 3)
+})
+
+test('Pass multiple batches where a > b', t => {
+  const o = optimizeDispenseWithLimit([54, 12])
+
+  t.is(o.length, 4)
+})
+
+test('Pass multiple batches where a = b', t => {
+  const o = optimizeDispenseWithLimit([40, 40])
+
+  t.is(o.length, 4)
+})
+
+test('Sum of optimized batches equals input', t => {
+  const o = optimizeDispenseWithLimit([54, 12])
+
+  t.is(_.sum(_.flatten(o)), 66)
+})
