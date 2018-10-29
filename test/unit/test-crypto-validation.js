809a23d19f8053b9886c27eac0f33f533649e729
Zoran Joka <zoran.joka@gmail.com>
2018-10-29 16:30:03 +0100

    Move bech32 validation to BTC, optimize validators, add more tests

--- test/unit/test-crypto-validation.js
+++ test/unit/test-crypto-validation.js
@@ -1,6 +1,6 @@
 import test from 'ava'
-import crypt0Validator from '../../lib/coins/validators'
-import {base58Opts as BTCBase58Opts, bech32Opts as BTCBech32Opts} from '../../lib/coins/btc'
+import cryptoValidator from '../../lib/coins/validators'
+import {parseUrl as parseBTCAddress, base58Opts as BTCBase58Opts} from '../../lib/coins/btc'
 import {base58Opts as LTCBase58Opts} from '../../lib/coins/ltc'
 import {base58Opts as DASHBase58Opts} from '../../lib/coins/dash'
 import {base58Opts as ZECBase58Opts} from '../../lib/coins/zec'
@@ -9,8 +9,8 @@ test('Should validate BTC P2PKH', t => {
   t.plan(2)
   const mainNetaddr = '1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2'
   const testNetaddr = 'mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, BTCBase58Opts)
-  const validatedTest = crypt0Validator.base58Validator('test', testNetaddr, BTCBase58Opts)
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, BTCBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, BTCBase58Opts)
   t.true(validatedMain)
   t.true(validatedTest)
 })
@@ -19,8 +19,8 @@ test('Should validate BTC P2SH', t => {
   t.plan(2)
   const mainNetaddr = '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy'
   const testNetaddr = '2MzQwSSnBHWHqSAqtTVQ6v47XtaisrJa1Vc'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, BTCBase58Opts)
-  const validatedTest = crypt0Validator.base58Validator('test', testNetaddr, BTCBase58Opts)
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, BTCBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, BTCBase58Opts)
   t.true(validatedMain)
   t.true(validatedTest)
 })
@@ -29,8 +29,8 @@ test('Should validate LTC P2PKH', t => {
   t.plan(2)
   const mainNetaddr = 'LVdPgVLae4mTeAdywWqmwWkJypAGcRkHy3'
   const testNetaddr = 'mhLpXMH9EGV7xFRg5gjKXFpoxaer7ehEeW'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, LTCBase58Opts)
-  const validatedTest = crypt0Validator.base58Validator('test', testNetaddr, LTCBase58Opts)
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, LTCBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, LTCBase58Opts)
   t.true(validatedMain)
   t.true(validatedTest)
 })
@@ -39,8 +39,8 @@ test('Should validate LTC P2SH', t => {
   t.plan(2)
   const mainNetaddr = '35tx7n3XhCc1TJorddFzixgAU1nUVCUMxm'
   const testNetaddr = '2NBsZNqVDUKtchDSnTJT72tUgbtLGqpkfVe'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, LTCBase58Opts)
-  const validatedTest = crypt0Validator.base58Validator('test', testNetaddr, LTCBase58Opts)
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, LTCBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, LTCBase58Opts)
   t.true(validatedMain)
   t.true(validatedTest)
 })
@@ -49,8 +49,8 @@ test('Should validate DASH P2PKH', t => {
   t.plan(2)
   const mainNetaddr = 'XuvPuKz7JPkyzWn8g7PNLjYVNdouedLc56'
   const testNetaddr = 'yh1csSCnZNRLqBgzHFUMVTHaW4TU74K28R'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, DASHBase58Opts)
-  const validatedTest = crypt0Validator.base58Validator('test', testNetaddr, DASHBase58Opts)
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, DASHBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, DASHBase58Opts)
   t.true(validatedMain)
   t.true(validatedTest)
 })
@@ -59,25 +59,28 @@ test('Should validate DASH P2SH', t => {
   t.plan(2)
   const mainNetaddr = '7Z5BvydGVzgbX9xqWEb1JtF9TkToG8htcV'
   const testNetaddr = '8ik6MdU1RprCq6YtTsHNkg8biMxZUyXo8q'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, DASHBase58Opts)
-  const validatedTest = crypt0Validator.base58Validator('test', testNetaddr, DASHBase58Opts)
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, DASHBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, DASHBase58Opts)
   t.true(validatedMain)
   t.true(validatedTest)
 })
 
 test('Should validate ZEC P2PKH', t => {
-  t.plan(2)
   const mainNetaddr = 't1ZYZS6ynUDbvht7vH3dMiM3rsAJ1p6EGWC'
-  const testNetaddr = 't295qeRQc3xhzgf7HQYLh3du7uWyrLmtQf6'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, ZECBase58Opts)
-  const validatedTest = crypt0Validator.base58Validator('test', testNetaddr, ZECBase58Opts)
+  const testNetaddr = 'tmKBPqa8qqKA7vrGq1AaXHSAr9vqa3GczzK'
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, ZECBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, ZECBase58Opts)
   t.true(validatedMain)
   t.true(validatedTest)
 })
 
 test('Should validate ZEC P2SH', t => {
+  t.plan(2)
   const mainNetaddr = 't3f3T3nCWsEpzmD35VK62JgQfFig74dV8C9'
-  const validatedMain = crypt0Validator.base58Validator('main', mainNetaddr, ZECBase58Opts)
+  const testNetaddr = 't295qeRQc3xhzgf7HQYLh3du7uWyrLmtQf6'
+  const validatedMain = cryptoValidator.base58Validator('main', mainNetaddr, ZECBase58Opts)
+  const validatedTest = cryptoValidator.base58Validator('test', testNetaddr, ZECBase58Opts)
+  t.true(validatedTest)
   t.true(validatedMain)
 })
 
@@ -85,8 +88,22 @@ test('Should validate BTC bech32', t => {
   t.plan(2)
   const mainNetaddr = 'bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq'
   const testNetaddr = 'tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx'
-  const validatedMain = crypt0Validator.bech32Validator('main', mainNetaddr, BTCBech32Opts)
-  const validatedTest = crypt0Validator.bech32Validator('test', testNetaddr, BTCBech32Opts)
-  t.true(validatedMain)
-  t.true(validatedTest)
+  const validatedMain = parseBTCAddress('main', mainNetaddr)
+  const validatedTest = parseBTCAddress('test', testNetaddr)
+  t.is(validatedMain, mainNetaddr)
+  t.is(validatedTest, testNetaddr)
+})
+
+test('Should fail length validation', t => {
+  const addr = '1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2'
+  const opts = BTCBase58Opts
+  opts.bufferLength = 10
+  const validated = cryptoValidator.base58Validator('main', addr, opts)
+  t.false(validated)
+})
+
+test('Should throw exception on invalid address', t => {
+  const addr = '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNL0'
+  const validated = cryptoValidator.base58Validator('main', addr, BTCBase58Opts)
+  t.false(validated)
 })
