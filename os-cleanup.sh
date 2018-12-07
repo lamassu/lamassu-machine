7628ec3ee1368d07ee29906bbf4fc14fbab61a62
Zoran <zoran.joka@symlink.rs>
2018-12-07 03:37:22 -0800

    add clear logs to os-cleanup script

--- os-cleanup.sh
+++ os-cleanup.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Stopping supervisor"
+supervisorctl stop all
+
 echo "Removing connection data and client keys"
 rm /opt/lamassu-machine/data/connection_info.json
 rm /opt/lamassu-machine/data/client.pem
 rm /opt/lamassu-machine/data/client.key
 
-echo "Restarting l-m"
-supervisorctl restart lamassu-machine
+echo "Clearing logs"
+rm /var/log/supervisor/lamassu-*
+
+echo "Restarting supervisor"
+supervisorctl restart all
 
 echo "Clearing command line history"
 history -cw
