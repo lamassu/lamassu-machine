e66c4dce9936036bd2dbd93204695aadde0c172c
littlenaut <littlenaut@protonmail.com>
2018-12-16 14:35:12 +0000

    Pulse all LEDs by default to ensure all units are operational

--- tools/led-test.js
+++ tools/led-test.js
@@ -3,6 +3,6 @@ const ledControl = require('../lib/ssuboard/led-control')
 const COLORS = ledControl.COLORS
 const LEDS = ledControl.LEDS
 
-ledControl.lightUp({type: 'pulse', color: COLORS.orange, range: LEDS.doorLeds})
+ledControl.lightUp({type: 'pulse', color: COLORS.orange, range: LEDS.allLeds})
 
 process.on('unhandledRejection', console.log)
