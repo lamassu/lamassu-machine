02119d60245ff2d1a4c0902e8da06059eef47f3e
littlenaut <littlenaut@protonmail.com>
2018-12-30 21:02:24 +0000

    Rename and improve l-m reset script
    - Renamed from os-cleanup.sh to reset.sh because we're only reseting
      state related with lamassu-machine.
    - Bash's history cleanup only works as expected when executed from the
      last login shell.

---
+++ reset.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Stopping lamassu processes and clearing their logs..."
+supervisorctl stop lamassu-audio lamassu-browser lamassu-machine lamassu-updater lamassu-watchdog
+supervisorctl clear lamassu-audio lamassu-browser lamassu-machine lamassu-updater lamassu-watchdog
+
+echo "Removing lamassu-machine persistent data..."
+rm -rf /opt/lamassu-machine/data/connection_info.json \
+       /opt/lamassu-machine/data/client.pem \
+       /opt/lamassu-machine/data/client.key \
+       /opt/lamassu-machine/data/machine.log \
+       /opt/lamassu-machine/data/u2f.json \
+       /opt/lamassu-machine/data/tx-db \
+
+touch /root/.bash_logout
+touch /home/debian/.bash_logout
+echo "history -c" > /root/.bash_logout
+echo "history -c" > /home/debian/.bash_logout
+
+echo "All done!"
