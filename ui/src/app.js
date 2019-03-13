5dce93da9c6022399486c2a04b7cdecb65a4891c
littlenaut <littlenaut@protonmail.com>
2019-03-13 22:53:41 +0000

    Normalise field that has the machine model designation

--- ui/src/app.js
+++ ui/src/app.js
@@ -131,8 +131,8 @@ function processData (data) {
   if (data.dispenseBatch) dispenseBatch(data.dispenseBatch)
   if (data.direction) setDirection(data.direction)
   if (data.operatorInfo) setOperatorInfo(data.operatorInfo)
-  if (data.machineVersion) setMachineVersion(data.machineVersion)
   if (data.hardLimitHours) setHardLimitHours(data.hardLimitHours)
+  if (data.cryptomatModel) setMachineVersion(data.cryptomatModel)
 
   if (data.context) {
     $('.js-context').hide()
