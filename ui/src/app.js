8a8828b97bfbf1b246f2c86d6d16005d26a86a66
Rafael Taranto <rafael.taranto5@gmail.com>
2019-04-29 23:15:44 -0300

    Add blacklist functionality

--- ui/src/app.js
+++ ui/src/app.js
@@ -534,6 +534,7 @@ $(document).ready(function () {
   setupButton('photo-scan-failed-cancel', 'bye')
   setupButton('photo-verification-failed-ok', 'cancelIdScan')
   setupButton('invalid-address-try-again', 'invalidAddressTryAgain')
+  setupButton('suspicious-address-start-over', 'idle')
 
   setupButton('sanctions-failure-ok', 'idle')
   setupButton('limit-reached-ok', 'idle')
