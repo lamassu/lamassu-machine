5f01ef9770d09ee95692983bf0622d78ccb3259b
Rafael Taranto <rafael.taranto5@gmail.com>
2019-07-03 14:33:51 +0100

    Remove opencv dependency, use v4l2camera + pico

--- bin/cam.js
+++ bin/cam.js
@@ -1,22 +1,13 @@
 const fs = require('fs')
 
 const scanner = require('../lib/scanner')
-const cam = require('@lamassu/camera-wrapper')
-const mockWrapper = require('../lib/mocks/camera-wrapper')
-
-const configuration = require('../lib/configuration')
-  .loadConfig({ 'mockCam': true, mockWrapper })
 
 const frameCb = function (err, frame) {
-  console.log('frame callback', {err, frame})
-  console.log('frame size ', cam.getFrameSize())
+  console.log('frame callback', { err, frame })
 
   console.log('writing result.jpg')
   fs.writeFileSync('result.jpg', frame)
 }
-
-console.log('configure scanner')
-scanner.config(configuration)
 
 console.log('starting scanner')
 scanner.scanPhotoCard(frameCb)
