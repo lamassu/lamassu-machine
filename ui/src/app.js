e6e53294c312dbbb2838e61e19c4783e2caf843d
Rafael Taranto <rafael.taranto@protonmail.com>
2019-11-20 14:22:45 +0000

    fix: properly clear terms screen timeout

--- ui/src/app.js
+++ ui/src/app.js
@@ -789,6 +789,10 @@ function setScreen (newScreen, oldScreen) {
 function setState (state, delay) {
   if (state === currentState) return
 
+  if (currentState === 'terms_screen') {
+    clearTermsConditionsTimeout()
+  }
+
   previousState = currentState
   currentState = state
 
