099c20de2fa4a031b2131f611b269459e8a90f18
Rafael Taranto <rafael.taranto@protonmail.com>
2019-11-28 14:53:14 +0000

    chore: add scripts to toggle cursor visibility

---
+++ tools/upboard-remove-cursor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo rm /etc/sddm.conf
+
+cat > sddm.conf << EOL
+[Autologin]
+User=ubilinux
+Session=lxqt.desktop
+
+[X11]
+ServerArguments=-nolisten tcp -nocursor
+EOL
+
+sudo mv sddm.conf /etc/
+
+sudo reboot
