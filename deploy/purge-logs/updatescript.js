fba11f40974ab0872c6cbee74090e51402830a4e
naconner <7396812+naconner@users.noreply.github.com>
2020-01-09 11:25:06 -0500

    chore: purge more logs

--- deploy/purge-logs/updatescript.js
+++ deploy/purge-logs/updatescript.js
@@ -9,9 +9,9 @@ var TIMEOUT = 10000;
 
 let purgeCommand = null
 if (hardwareCode === 'aaeon')
-  purgeCommand = 'rm /var/lib/lamassu-machine/log/*; rm /var/lib/lamassu-machine/tx-db/*'
+  purgeCommand = 'rm /var/lib/lamassu-machine/log/*; rm /var/lib/lamassu-machine/tx-db/*; rm /var/log/upstart/lamassu-*.log.*; truncate -s 0 /var/log/upstart/lamassu-*.log'
 else if (hardwareCode === 'ssuboard' || hardwareCode === 'upboard')
-  purgeCommand = 'rm /opt/lamassu-machine/data/log/*; rm /opt/lamassu-machine/data/tx-db/*'
+  purgeCommand = 'rm /opt/lamassu-machine/data/log/*; rm /opt/lamassu-machine/data/tx-db/*; rm /var/log/supervisor/lamassu-*.*.log.*; truncate -s 0 /var/log/supervisor/lamassu-*.*.log'
 else purgeCommand = ''
 
 function command(cmd, cb) {
