347ab878f315bc9662c1285ac45b33512a1981e6
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2020-01-29 05:01:46 -0600

    test: some batchDispense tests

--- test/unit/test-brain-batch-dispense.js
+++ test/unit/test-brain-batch-dispense.js
@@ -1,15 +1,46 @@
 import test from 'ava'
+import BillDispenser from '../../lib/mocks/billdispenser'
 import Brain from '../../lib/brain'
 import Configuration from '../../lib/configuration'
+import optimizeDispense from '../../lib/dispense-optimizer'
 
-Brain.prototype.selectBillValidatorClass = function selectBillValidatorClass() {
-  return require('../../lib/mocks/id003')
+const { billDispenser } = Configuration.loadConfig({})
+
+const thisArg = {
+  browser: () => ({ send: () => { } }),
+  billDispenser: BillDispenser.factory(billDispenser)
 }
 
-test('new Brain', t => {
-  var overrides = JSON.parse('{"_":[], "mockBTC":"1EyE2nE4hf8JVjV51Veznz9t9vTFv8uRU5", "mockBv":"/dev/pts/7", "mockTrader":true, "mockCam":true, "mockBillDispenser":true, "brain": { "checkIdle":2000, "idleTime":10000, "exitTime":20000} }');
-  const config = Configuration.loadConfig(overrides)
-  const brain = new Brain(config)
+test('batch dispense [20,0] limit 20', async t => {
+  const notesToDispense = optimizeDispense([20, 0], 20)
 
-  t.true(false)
+  const initialParams = {
+    batchAmount: notesToDispense.length,
+    currentBatch: 1,
+    dispenseRecords: [
+      { dispensed: 0, rejected: 0 }, { dispensed: 0, rejected: 0 }
+    ]
+  }
+
+  const { dispenseRecords, error } = await Brain.prototype._batchDispense.apply(thisArg, [notesToDispense, initialParams])
+  t.falsy(error, 'No error should happen')
+  t.deepEqual(dispenseRecords, [{ dispensed: 20, rejected: 0 }, { dispensed: 0, rejected: 0 }], 'Dispenses all 20 notes on first batch');
+})
+
+
+test('batch dispense [0,20] limit 20', async t => {
+
+  const notesToDispense = optimizeDispense([0, 20], 20)
+
+  const initialParams = {
+    batchAmount: notesToDispense.length,
+    currentBatch: 1,
+    dispenseRecords: [
+      { dispensed: 0, rejected: 0 }, { dispensed: 0, rejected: 0 }
+    ]
+  }
+
+  const { dispenseRecords, error } = await Brain.prototype._batchDispense.apply(thisArg, [notesToDispense, initialParams])
+  t.falsy(error, 'No error should happen')
+  t.deepEqual(dispenseRecords, [{ dispensed: 0, rejected: 0 }, { dispensed: 20, rejected: 0 }], 'Dispenses all 20 notes on first batch');
 })
