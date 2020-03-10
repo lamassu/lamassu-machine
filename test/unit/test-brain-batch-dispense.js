bb381657154c7f4690f9e8738a41e224d4187d01
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2020-03-10 03:57:20 -0600

    test: add batch dispense tests for the cases covered by optimize batch test, using the loop

--- test/unit/test-brain-batch-dispense.js
+++ test/unit/test-brain-batch-dispense.js
@@ -59,7 +59,7 @@ test('batch dispense [0,101] limit 20', async t => {
   })
 
   const batches = notesToDispense.reduce((acc, notes) => acc.then(it => {
-    const { value: batch, dispenseRecords, currentBatch, error } = it;
+    const { batch, dispenseRecords, currentBatch, error } = it;
 
     if (currentBatch > 1 && currentBatch < 7) {
       t.falsy(error, 'No error should happen')
@@ -71,8 +71,68 @@ test('batch dispense [0,101] limit 20', async t => {
   }), initialParams)
 
 
-  const { value: lastBatch, dispenseRecords, error } = await batches
+  const { batch: lastBatch, dispenseRecords, error } = await batches
   t.falsy(error, 'No error should happen')
   t.deepEqual(lastBatch, [{ dispensed: 0, rejected: 0 }, { dispensed: 1, rejected: 0 }], 'Last batch should dispense as expected');
   t.deepEqual(dispenseRecords, [{ dispensed: 0, rejected: 0 }, { dispensed: 101, rejected: 0 }], 'Cumulative dispensed is as expected');
 })
+
+test('Pass only one batch [2, 6]', t => loopTest(t, [2, 6]))
+test('Pass multiple batches where a < b [4, 43]', t => loopTest(t, [4, 43]))
+test('Pass multiple batches where a = b [10, 10], 20', t => loopTest(t, [10, 10], 20))
+test('Pass multiple batches where a > b [54, 12]', t => loopTest(t, [54, 12]))
+test('Pass multiple batches where a = b [40, 40]', t => loopTest(t, [40, 40]))
+
+const loopTest = async (t, [a, b], limit = 20) => {
+
+  const notesToDispense = optimizeDispense([a, b], limit)
+  const initialParams = Promise.resolve({
+    batchAmount: notesToDispense.length,
+    currentBatch: 1,
+    dispenseRecords: [
+      { dispensed: 0, rejected: 0 }, { dispensed: 0, rejected: 0 }
+    ]
+  })
+
+  const batches = notesToDispense.reduce((acc, notes) => acc.then(async it => {
+    const next = Brain.prototype._batchDispense.apply(thisArg, [notes, it])
+    const { batch, dispenseRecords, currentBatch: nextBatch, error } = await next;
+    t.falsy(error, 'No error should happen')
+    if (nextBatch <= Math.ceil(a / limit)) {
+      // ceil batch is the first one to have a mixture of a and b
+      // i.e. all batches before ceil, have limit amount of a and 0 amount of b
+      t.deepEqual(batch, [{ dispensed: limit, rejected: 0 }, { dispensed: 0, rejected: 0 }], 'Current batch dispenses limit amount of a and 0 b');
+      t.deepEqual(dispenseRecords, [{ dispensed: limit * (nextBatch - 1), rejected: 0 }, { dispensed: 0, rejected: 0 }], 'Cumulative dispensed includes no b');
+    }
+    else if (nextBatch === 1 + Math.ceil(a / limit)) {
+      // a - (limit * (currentBatch - 1))
+      // total amount of a to be dispensed, minus cumulative a dispensed up to the previous batch
+      const lastA = a % limit || limit
+      const firstB = Math.min(b, limit - lastA)
+
+      t.deepEqual(batch, [{ dispensed: lastA, rejected: 0 }, { dispensed: firstB, rejected: 0 }], 'Current batch dispenses residual amount of a and compliments with b');
+      t.deepEqual(dispenseRecords, [{ dispensed: a, rejected: 0 }, { dispensed: firstB, rejected: 0 }], 'All a already dispensed, but only first b');
+    } else if (nextBatch <= notesToDispense.length) {
+      // given that this is not the lastBatch, limit amount of b must be dispensed (also 0 amount of a)
+      const lastA = a % limit || limit
+      const firstB = Math.min(b, limit - lastA)
+      t.deepEqual(batch, [{ dispensed: 0, rejected: 0 }, { dispensed: limit, rejected: 0 }], 'Current batch dispenses limit amount of b; no a');
+      t.deepEqual(dispenseRecords, [{ dispensed: a, rejected: 0 }, { dispensed: firstB + limit * (nextBatch - 1 - Math.ceil(a / limit)), rejected: 0 }], 'All a already dispensed, but only first b + a few limit batches of b');
+    } else if (a + b > limit) {
+      // last batch
+      // corner case: 
+      // when limit > a + b (i.e. there is only one batch), lastB makes no sense 
+      const lastA = a % limit || limit
+      const fB = Math.min(b, limit - lastA)
+      const lastB = (b - fB) % limit || limit
+      t.deepEqual(batch[1], { dispensed: lastB, rejected: 0 }, 'Last batch dispenses correct amount of b');
+    }
+
+    return Promise.resolve(next)
+  }), initialParams)
+
+
+  const { dispenseRecords, error } = await batches
+  t.falsy(error, 'No error should happen')
+  t.deepEqual(dispenseRecords, [{ dispensed: a, rejected: 0 }, { dispensed: b, rejected: 0 }], 'Cumulative dispensed is as expected');
+}
