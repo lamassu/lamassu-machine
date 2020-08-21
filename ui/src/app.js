d0055559dad6b31d2a4b5b7fe328f8a55d890ab2
Taranto <rafael.taranto@protonmail.com>
2020-08-21 11:32:55 +0100

    feat: new compliance triggers

--- ui/src/app.js
+++ ui/src/app.js
@@ -1380,7 +1380,7 @@ function sendOnly (reason) {
     transactionLimit: locale.translate('Transaction limit reached').fetch(),
     validatorError: locale.translate('Error in validation').fetch(),
     lowBalance: locale.translate("We're out of coins!").fetch(),
-    blockedCustomer: locale.translate('Transaction limit reached')
+    blockedCustomer: locale.translate('Transaction limit reached').fetch()
   }
 
   // If no reason provided defaults to lowBalance
