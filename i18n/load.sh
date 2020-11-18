cd3d63276f97996886cf009fbec6a30c1164795b
Neal <neal@lamassu.is>
2020-11-18 13:11:24 -0500

    chore: add Georgian translation, clean up i18n

--- i18n/load.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mv $HOME/Downloads/lamassu-bitcoin-machine-ui.zip .
-unzip -qq lamassu-bitcoin-machine-ui.zip -d locales
-ruby to_json.rb
-mv i18n.js ../ui/js/locales.js
-rm -rf locales/*
