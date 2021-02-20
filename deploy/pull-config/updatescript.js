60fa90d124292612a032fd89fedcaa9e062b84d5
Neal <neal@lamassu.is>
2021-02-20 13:13:19 -0500

    fix: add isAaeon variable, no semicolons

--- deploy/pull-config/updatescript.js
+++ deploy/pull-config/updatescript.js
@@ -1,43 +1,44 @@
-'use strict';
+'use strict'
 
-const https = require('https');
-const fs = require('fs');
-const async = require('./async');
-const cp = require('child_process');
-const report = require('./report').report;
+const https = require('https')
+const fs = require('fs')
+const async = require('./async')
+const cp = require('child_process')
+const report = require('./report').report
 
-const hardwareCode = process.argv[2];
+const hardwareCode = process.argv[2]
+const isAaeon = hardwareCode === 'aaeon'
 
-const clientFolder = hardwareCode === 'aaeon' ? '/var/lib/lamassu-machine' : '/opt/lamassu-machine/data'
-const lmFolder = hardwareCode === 'aaeon' ? '/opt/apps/machine/lamassu-machine' : '/opt/lamassu-machine'
-const lamassuCertFolder = hardwareCode === 'aaeon' ? '/var/lib/lamassu-machine' : '/opt/certs'
+const clientFolder = isAaeon ? '/var/lib/lamassu-machine' : '/opt/lamassu-machine/data'
+const lmFolder = isAaeon ? '/opt/apps/machine/lamassu-machine' : '/opt/lamassu-machine'
+const lamassuCertFolder = isAaeon ? '/var/lib/lamassu-machine' : '/opt/certs'
 
-const key = fs.readFileSync(`${clientFolder}/client.key`);
-const cert = fs.readFileSync(`${clientFolder}/client.pem`);
-const ca = fs.readFileSync(`${lamassuCertFolder}/lamassu.pem`);
+const key = fs.readFileSync(`${clientFolder}/client.key`)
+const cert = fs.readFileSync(`${clientFolder}/client.pem`)
+const ca = fs.readFileSync(`${lamassuCertFolder}/lamassu.pem`)
 
-const TIMEOUT = 600000;
+const TIMEOUT = 600000
 
 const deviceConfig = `${lmFolder}/device_config.json`
-const udevRules = hardwareCode === 'aaeon' ? '/etc/udev/rules.d/99-douro.rules' : ('gaia' ? '/etc/udev/rules.d/99-gaia.rules' : '/etc/udev/rules.d/udevssu.rules')
+const udevRules = isAaeon ? '/etc/udev/rules.d/99-douro.rules' : ('gaia' ? '/etc/udev/rules.d/99-gaia.rules' : '/etc/udev/rules.d/udevssu.rules')
 
 function command(cmd, cb) {
   cp.exec(cmd, {timeout: TIMEOUT}, function(err) {
-    cb(err);
-  });
+    cb(err)
+  })
 }
 
 function tailFile(file, cb) {
   fs.exists(file, function(exists) {
     if (!exists) {
-      report(file + ' does not exist', null, cb);
-      return;      
+      report(file + ' does not exist', null, cb)
+      return
     }
     fs.stat(file, function(err, stats) {
-      if (err) return report(err, null, cb);
+      if (err) return report(err, null, cb)
       var opts = {
         start: stats.size - 1024000
-      };
+      }
 
       var httpsOptions = {
         host: 'updates.lamassu.is',
@@ -53,16 +54,16 @@ function tailFile(file, cb) {
         headers: {
           'Content-Type': 'application/gzip'
         }
-      };
-      httpsOptions.agent = new https.Agent(httpsOptions);
+      }
+      httpsOptions.agent = new https.Agent(httpsOptions)
 
       var req = https.request(httpsOptions, function(res) {
-        res.resume();
-        res.on('end', cb);
-      });
-      fs.createReadStream(file, opts).pipe(req);
-    });    
-  });
+        res.resume()
+        res.on('end', cb)
+      })
+      fs.createReadStream(file, opts).pipe(req)
+    })
+  })
 }
 
 async.series([
@@ -70,5 +71,5 @@ async.series([
   async.apply(tailFile, '/tmp/config.tar.gz'),
   async.apply(report, null, 'finished.')
 ], function(err) {
-  if (err) throw err;
-});
+  if (err) throw err
+})
