4d0ec25a08937751d78f0e67a4b72c4810bc94d6
csrapr <26280794+csrapr@users.noreply.github.com>
2021-05-14 15:36:55 +0100

    feat: Arbitrary Compliance [Text | Numerical]
    feat: Arbitrary Compliance - [List]

--- ui/js/keypad.js
+++ ui/js/keypad.js
@@ -3,7 +3,8 @@ var TIMEOUT = 120000
 var LENGTHS = {
   phoneNumber: 15,
   code: 10,
-  usSsn: 9
+  usSsn: 9,
+  custom: Infinity
 }
 
 var Keypad = function (keypadId, opts, callback) {
@@ -15,6 +16,9 @@ var Keypad = function (keypadId, opts, callback) {
   this.opts = opts
   this.callback = callback
   this.timeoutRef = null
+
+  if (this.opts.constraint && this.opts.constraint === 'length') LENGTHS.custom = this.opts.maxLength
+
   var self = this
 
   function keyHandler (e) {
@@ -26,9 +30,16 @@ var Keypad = function (keypadId, opts, callback) {
 
     if (target.hasClass('enter')) {
       self.deactivate()
+      let result = self.result
 
-      const phoneResult = libphonenumber.parsePhoneNumberFromString(self.result, self.opts.country)
-      const result = self.type === 'phoneNumber' && phoneResult ? phoneResult.number : self.result
+      if (self.type === 'phoneNumber') {
+        const phoneResult = libphonenumber.parsePhoneNumberFromString(self.result, self.opts.country)
+        if (phoneResult) result = phoneResult
+      }
+
+      if (self.type === 'custom' && self.opts.constraint === 'date') {
+        result = `${self.result.slice(0, 4)}-${self.result.slice(4, 6)}-${self.result.slice(6, 8)}`
+      }
 
       self.reset()
       return self.callback(result)
@@ -76,7 +87,7 @@ Keypad.prototype.setCountry = function setCountry (country) {
 }
 
 Keypad.prototype.reset = function reset () {
-  this.keypad.find('.box').text('')
+  this.keypad.find('.box').text(this.opts.constraint === 'date' ? 'YYYY-MM-DD' : '')
   this.count = 0
   this.result = ''
   this.keypad.find('.phone-separator').addClass('hidden')
@@ -92,7 +103,7 @@ Keypad.prototype.backspace = function backspace () {
 
   this.result = this.result.substring(0, this.result.length - 1)
 
-  var display = getDisplay(this.result, this.type, this.opts.country)
+  var display = getDisplay(this.result, this.type, this.opts)
 
   if (!display) {
     this.keypad.find('.phone-separator').addClass('hidden')
@@ -123,13 +134,13 @@ function ssnFormat (ssn) {
   }
 
   if ((ssn.length > 5)) {
-    return ssn.substr(0, 3) + '-' + ssn.substr(3, 2) + '-' + ssn.substr(5);
+    return ssn.substr(0, 3) + '-' + ssn.substr(3, 2) + '-' + ssn.substr(5)
   }
 }
 
 function isValidSsn (ssn) {
-  return ssn && 
-    ssn.length === LENGTHS.usSsn && 
+  return ssn &&
+    ssn.length === LENGTHS.usSsn &&
     ssn.substr(0, 1) !== '9' &&
     ssn.substr(0, 3) !== '000' &&
     ssn.substr(0, 3) !== '666' &&
@@ -137,12 +148,27 @@ function isValidSsn (ssn) {
     ssn.substr(5) !== '0000'
 }
 
-function getDisplay (result, type, country) {
+function customFormat (text, opts) {
+  if (opts.constraint === 'date') {
+    const year = text.slice(0, 4).padEnd(4, 'Y')
+    const month = text.slice(4, 6).padEnd(2, 'M')
+    const day = text.slice(6, 8).padEnd(2, 'D')
+    return `${year}-${month}-${day}`
+  }
+  return text
+}
+
+function validateDate (text) {
+  if (text.length !== 8) return false
+  return !!Date.parse(`${text.slice(0, 4)}-${text.slice(4, 6)}-${text.slice(6, 8)}`)
+}
+
+function getDisplay (result, type, opts) {
   if (!result) return result
 
-  if (type === 'phoneNumber') return new libphonenumber.AsYouType(country).input(result)
+  if (type === 'phoneNumber') return new libphonenumber.AsYouType(opts.country).input(result)
   if (type === 'usSsn') return ssnFormat(result)
-
+  if (type === 'custom') return customFormat(result, opts)
   return result
 }
 
@@ -151,7 +177,7 @@ Keypad.prototype._keyPress = function _keyPress (target) {
   var numeral = target.text()
   this.result += numeral
 
-  var display = getDisplay(this.result, this.type, this.opts.country)
+  var display = getDisplay(this.result, this.type, this.opts)
 
   if (display) {
     this.keypad.find('.phone-separator').removeClass('hidden')
@@ -169,5 +195,40 @@ Keypad.prototype._keyPress = function _keyPress (target) {
 
   if (this.type === 'usSsn' && isValidSsn((this.result))) {
     this.keypad.find('.enter')[0].disabled = false
+  }
+
+  if (this.type === 'custom' && this.opts.constraint === 'date') {
+    this.keypad.find('.enter')[0].disabled = !validateDate(this.result)
+  }
+}
+
+Keypad.prototype.setOpts = function setOpts (newOpts) {
+  this.opts = newOpts
+  if (!this.opts.constraint) return
+  switch (this.opts.constraint) {
+    case 'length':
+      LENGTHS.custom = this.opts.maxLength
+      break
+    case 'date':
+      LENGTHS.custom = 8
+      break
+    default:
+      break
+  }
+}
+
+Keypad.prototype.setOpts = function setOpts (newOpts) {
+  this.opts = newOpts
+  if (this.opts.constraint) {
+    switch (this.opts.constraint) {
+      case 'length':
+        LENGTHS.custom = this.opts.maxLength
+        break
+      case 'date':
+        LENGTHS.custom = 8
+        break
+      default:
+        break
+    }
   }
 }
