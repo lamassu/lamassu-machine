72b6aa5cf114a72354d119ee3e2c6afc3dbab285
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-06-10 01:49:49 +0100

    fix: coinUtils import

--- tools/printer-tests.js
+++ tools/printer-tests.js
@@ -2,7 +2,7 @@ const minimist = require('minimist')
 const SerialPort = require('serialport')
 
 const BN = require('../lib/bn')
-const coinUtils = require('lamassu-coins').utils
+const { utils: coinUtils } = require('lamassu-coins')
 const printerLoader = require('../lib/printer/loader')
 
 const deviceConfig = require('../device_config.json')
