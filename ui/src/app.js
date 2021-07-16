d590f62c05e500e584769c3faae222de9cf40d72
José Oliveira <josepfo.w@gmail.com>
2021-07-16 12:35:40 +0100

    fix: add try-catch for non-breaking translations as well

--- ui/src/app.js
+++ ui/src/app.js
@@ -409,19 +409,27 @@ function setupCoinsButtons () {
 }
 
 function setCryptoBuy (coin) {
-  const cashIn = $('.cash-in')
-  const translatedCoin = locale.translate(coin.display).fetch()
-  const buyStr = locale.translate('Buy<br/>%s').fetch(translatedCoin)
+  try {
+    const cashIn = $('.cash-in')
+    const translatedCoin = locale.translate(coin.display).fetch()
+    const buyStr = locale.translate('Buy<br/>%s').fetch(translatedCoin)
 
-  cashIn.html(buyStr)
+    cashIn.html(buyStr)
+  } catch (error) {
+    console.error('Error while translating: ', error)
+  }
 }
 
 function setCryptoSell (coin) {
-  const cashOut = $('.cash-out')
-  const translatedCoin = locale.translate(coin.display).fetch()
-  const sellStr = locale.translate('Sell<br/>%s').fetch(translatedCoin)
+  try {
+    const cashOut = $('.cash-out')
+    const translatedCoin = locale.translate(coin.display).fetch()
+    const sellStr = locale.translate('Sell<br/>%s').fetch(translatedCoin)
 
-  cashOut.html(sellStr)
+    cashOut.html(sellStr)
+  } catch (error) {
+    console.error('Error while translating: ', error)
+  }
 }
 
 function switchCoin (coin) {
@@ -922,17 +930,17 @@ function setOperatorInfo (operator) {
   }
 }
 
-function setHardLimit(limits) {
+function setHardLimit (limits) {
   const component = $('#hard-limit-hours')
-
-  if (limits.hardLimitWeeks >= 1) {
-    return component.text(locale.translate('Please come back in %s weeks').fetch(limits.hardLimitWeeks))
-  }
-
-  if (limits.hardLimitDays >= 1) {
-    return component.text(locale.translate('Please come back in %s days and %s hours').fetch(limits.hardLimitDays, limits.hardLimitHours))
-  }
   try {
+    if (limits.hardLimitWeeks >= 1) {
+      return component.text(locale.translate('Please come back in %s weeks').fetch(limits.hardLimitWeeks))
+    }
+
+    if (limits.hardLimitDays >= 1) {
+      return component.text(locale.translate('Please come back in %s days and %s hours').fetch(limits.hardLimitDays, limits.hardLimitHours))
+    }
+
     component.text(locale.translate('Please come back in %s hours').fetch(limits.hardLimitHours))
   } catch (error) {
     console.error('Error while translating: ', error)
@@ -1822,13 +1830,17 @@ function setAvailablePromoCodes (areThereAvailablePromoCodes) {
 
 function setCurrentDiscount (currentDiscount) {
   if (currentDiscount > 0) {
-    const successMessage = '✔ ' + locale.translate('Promo code added (%s discount)').fetch(`${currentDiscount}%`)
-    $('#insert-first-bill-promo-button').hide()
-    $('#choose-fiat-promo-button').hide()
-    $('#insert-first-bill-code-added').html(successMessage)
-    $('#choose-fiat-code-added').html(successMessage)
-    $('#insert-first-bill-code-added').show()
-    $('#choose-fiat-code-added').show()
+    try {
+      const successMessage = '✔ ' + locale.translate('Promo code added (%s discount)').fetch(`${currentDiscount}%`)
+      $('#insert-first-bill-promo-button').hide()
+      $('#choose-fiat-promo-button').hide()
+      $('#insert-first-bill-code-added').html(successMessage)
+      $('#choose-fiat-code-added').html(successMessage)
+      $('#insert-first-bill-code-added').show()
+      $('#choose-fiat-code-added').show()
+    } catch (error) {
+      console.error('Error while translating: ', error)
+    }
   } else {
     $('#insert-first-bill-promo-button').show()
     $('#choose-fiat-promo-button').show()
