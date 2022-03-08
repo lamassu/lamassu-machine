fb266f556bfdc3ed2d326b9d70055faff5792fc6
Sérgio Salgado <sergiodiassalgado@gmail.com>
2022-03-08 16:58:14 +0000

    chore: downgrade nix-shell to use node 6 fix: use of spread operator

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
 with import (fetchTarball {
   name = "nixpkgs-19.03";
-  url = https://github.com/NixOS/nixpkgs-channels/archive/f52505fac8c82716872a616c501ad9eff188f97f.tar.gz;
-  sha256 = "0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
+  url = https://github.com/NixOS/nixpkgs-channels/archive/69fabc286f745bca37a8cbead3665de31758e778.tar.gz;
+  sha256 = "0mkqv8d4vh6r1b34n7ik0gpzqhf60sj1masd89jndlckvklqh69j";
 }) {};
 
 
 stdenv.mkDerivation {
     name = "node";
     buildInputs = [
-      nodejs-8_x
+      nodejs-6_x
       openssl_1_0_2
       python2Full
       entr
