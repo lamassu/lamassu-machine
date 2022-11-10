9511256125d9e35414bf6ba955a39eae3138ec70
Sérgio Salgado <sergiodiassalgado@gmail.com>
2022-11-10 02:35:55 +0000

    feat: add external validation compliance flow feat: add external validation info screen and trader module
    feat: add external compliance review verification
    
    fix: remove hacky custom info requests verification
    feat: add external validation status verification
    
    fix: external compliance info UI
    fix: add screen timeout for external compliance
    fix: small fixes when checking for trigger compliance

--- ui/src/app.js
+++ ui/src/app.js
@@ -291,6 +291,9 @@ function processData (data) {
     case 'invalidAddress':
       invalidAddress(data.lnInvoiceTypeError)
       break
+    case 'externalCompliance':
+      externalCompliance(data.externalComplianceUrl)
+      break
     default:
       if (data.action) setState(window.snakecase(data.action))
   }
@@ -969,6 +972,8 @@ $(document).ready(function () {
     customRequirementTextKeyboard.setInputBox('.text-input-field-1')
   })
 
+  setupButton('external-validation-ok', 'finishBeforeSms')
+
   touchEvent(document.getElementById('change-language-section'), () => {
     if (_primaryLocales.length === 2) {
       setLocale(otherLocale())
@@ -1246,7 +1251,8 @@ function setDirection (direction) {
     $('.custom_permission_state'),
     $('.custom_permission_screen2_numerical_state'),
     $('.custom_permission_screen2_text_state'),
-    $('.custom_permission_screen2_choiceList_state')
+    $('.custom_permission_screen2_choiceList_state'),
+    $('.external_compliance_state')
   ]
   states.forEach(it => {
     setUpDirectionElement(it, direction)
@@ -2216,4 +2222,9 @@ function setReceiptPrint (receiptStatus, smsReceiptStatus) {
       $(`#${className}-cash-in-fail-message`).removeClass('hide')
       break
   }
+}
+
+function externalCompliance (url) {
+  setTimeout(() => qrize(url, $('#qr-code-external-validation'), CASH_OUT_QR_COLOR), 1000)
+  return setScreen('external_compliance')
 }
