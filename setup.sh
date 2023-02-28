1cdaf6bd26211c550fca71059d9068b8eb7666b0
Taranto <rafael.taranto@protonmail.com>
2023-02-28 10:01:23 +0000

    chore: simplify build

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ echo "Setting up..."
 mkdir -p data/log
 mkdir -p ui/css/fonts
 
-cp device_config.sample.json device_config.json
+cp mock_data/device_config.sample.json device_config.json
 
 if [ ! -e "licenses.json" ]; then
-    cp licenses.sample.json licenses.json
+    cp mock_data/licenses.sample.json licenses.json
 fi
 
 echo
