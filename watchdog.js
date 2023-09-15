91fa01211dd7ffeec90e6ed4053b4f8f84869927
Rafael Taranto <rafael.taranto@protonmail.com>
2023-09-15 18:43:23 +0100

    fix: done.txt path and download folder

--- watchdog.js
+++ watchdog.js
@@ -9,7 +9,7 @@ var watchdogInfoLoader = require('./lib/watchdog-info')
 
 require('./lite-logging')
 
-var BASE = '/opt/lamassu-updates'
+var BASE = '/opt/lamassu-updates/extract'
 var DONE_PATH = BASE + '/done.txt'
 var SCRIPT_PATH = BASE + '/package/updatescript.js'
 var RUNNING_PATH = BASE + '/running.txt'
