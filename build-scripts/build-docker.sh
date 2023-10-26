bb42f088463202eb1f729ecebe76aaad417e3af1
Taranto <rafael.taranto@protonmail.com>
2023-10-26 11:05:41 +0100

    chore: review changes

--- build-scripts/build-docker.sh
+++ build-scripts/build-docker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 sed "s/WebSocket(.*)/WebSocket('ws:\/\/' + location.hostname + '\/ws')/g" ui/js/app.js > ui/js/docker-app.js
-sed "s/app.js/docker-app.js/g" ui/start.html > ui/start-docker.html
-sed -i "s/..\/node_modules\///g"  ui/start-docker.html
-sed "s/app.js/test-app.js/g" ui/start.html > ui/start-test.html
-sed -i "s/..\/node_modules\///g"  ui/start-test.html
+sed 's/app.js/docker-app.js/g; s/..\/node_modules\///g;' ui/start.html > ui/start-docker.html
+sed 's/app.js/test-app.js/g; s/..\/node_modules\///g;' ui/start.html > ui/start-test.html
