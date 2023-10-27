5629c63454b4f3d40495a9b10ac3f853b5e2fd61
Neal <neal@lamassu.is>
2023-10-27 15:04:34 -0400

    chore: fix gmuk1 screen calibration

--- hardware/system/genmega/gmuk1/calibrate-screen.sh
+++ hardware/system/genmega/gmuk1/calibrate-screen.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
-xrandr -o right # normal, inverted, left, right
+xrandr -o left # normal, inverted, left, right
 identityMat='1 0 0 0 1 0 0 0 1'
 leftRotateMat='0 -1 1 1 0 0 0 0 1'
 rightRotateMat='0 1 0 -1 0 1 0 0 1'
-xinput set-prop 'ILITEK ILITEK-TP' --type=float 'Coordinate Transformation Matrix' $rightRotateMat
+xinput set-prop 'Silicon Works Multi-touch Device' --type=float 'Coordinate Transformation Matrix' $leftRotateMat
 xset s off
 xset s noblank
 xset -dpms
