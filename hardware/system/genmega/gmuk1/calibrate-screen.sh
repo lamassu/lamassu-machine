c306a0e2233ed387eaab09ea162ec291829ce832
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2023-11-03 13:15:40 -0400

    fix: account for both types of GMUK1 screens

--- hardware/system/genmega/gmuk1/calibrate-screen.sh
+++ hardware/system/genmega/gmuk1/calibrate-screen.sh
@@ -4,6 +4,7 @@ identityMat='1 0 0 0 1 0 0 0 1'
 leftRotateMat='0 -1 1 1 0 0 0 0 1'
 rightRotateMat='0 1 0 -1 0 1 0 0 1'
 xinput set-prop 'Silicon Works Multi-touch Device' --type=float 'Coordinate Transformation Matrix' $leftRotateMat
+xinput set-prop 'Silicon Works Multi-touch SW4101C' --type=float 'Coordinate Transformation Matrix' $leftRotateMat
 xset s off
 xset s noblank
 xset -dpms
