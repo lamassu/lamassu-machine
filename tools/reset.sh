ac9b4f86d05402300f9e9f63c91756c09e42f8ee
siiky <github-siiky@net-c.cat>
2023-11-28 16:04:33 +0000

    feat: update `reset.sh` script

--- tools/reset.sh
+++ tools/reset.sh
@@ -2,32 +2,15 @@
 set -e
 
 echo "Stopping lamassu processes and clearing their logs..."
-supervisorctl stop lamassu-audio lamassu-browser lamassu-machine lamassu-updater lamassu-watchdog
-supervisorctl clear lamassu-audio lamassu-browser lamassu-machine lamassu-updater lamassu-watchdog
+supervisorctl stop all
+supervisorctl clear all
 
 echo "Removing lamassu-machine persistent data..."
-rm -rf /opt/lamassu-machine/data/connection_info.json \
-       /opt/lamassu-machine/data/client.pem \
-       /opt/lamassu-machine/data/client.key \
-       /opt/lamassu-machine/data/machine.log \
-       /opt/lamassu-machine/data/u2f.json \
-       /opt/lamassu-machine/data/tx-db \
-       /opt/lamassu-machine/data/operator-info.json \
-       /opt/lamassu-machine/data/machine-info.json \
+rm -rf /opt/lamassu-machine/data/*
 
-
-if [ -d "/home/machine" ]; then
-  touch /home/machine/.bash_logout
-  echo "history -c" > /home/machine/.bash_logout
-fi
-
-if [ -d "/home/debian" ]; then
-  touch /home/debian/.bash_logout
-  echo "history -c" > /home/debian/.bash_logout
-fi
-
-touch /root/.bash_logout
-echo "history -c" > /root/.bash_logout
+for dir in '/home/machine' '/home/lamassu' '/root'; do
+  [ -d "${dir}" ] && echo "history -c" > "${dir}/.bash_logout"
+done
 
 echo > /etc/udev/rules.d/70-persistent-net.rules
 
