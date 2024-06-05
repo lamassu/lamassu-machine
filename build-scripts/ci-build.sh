bb8e67c2248837cea5b25996dccb92d8fb45b692
Rafael Taranto <rafael.taranto@protonmail.com>
2024-06-05 18:11:32 +0100

    feat: add cache to github action

--- build-scripts/ci-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build -t build:latest -f build-scripts/Dockerfile.ci .
-
-id=$(docker create build)
-docker cp $id:/lamassu/lamassu-machine/build/codebase/update.tar ./update.tar
-docker rm -v $id
