7d670d3613f83f24832749c5aefb0a88d8c321cc
siiky <github-siiky@net-c.cat>
2024-07-26 11:00:35 +0100

    feat: add `up4000` to `machineWithMultipleCodes`

--- deploy/codebase/lamassu-machine-manager.js
+++ deploy/codebase/lamassu-machine-manager.js
@@ -12,7 +12,7 @@ const newPath = process.argv[4];
 const basePath = newPath ? '/opt/lamassu-updates/extract' : '/tmp/extract'
 const packagePath = `${basePath}/package/subpackage`
 
-const machineWithMultipleCodes = ['upboard', 'coincloud', 'generalbytes', 'genmega']
+const machineWithMultipleCodes = ['upboard', 'up4000', 'coincloud', 'generalbytes', 'genmega']
 
 const path = machineWithMultipleCodes.includes(hardwareCode) ?
   `${packagePath}/hardware/${hardwareCode}/${machineCode}` :
