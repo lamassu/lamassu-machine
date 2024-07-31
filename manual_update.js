1ac5f6e899074fe5e41bc23d339db4daff04d7c6
siiky <github-siiky@net-c.cat>
2024-07-31 17:24:53 +0100

    refactor: simplify script and fix path building

--- manual_update.js
+++ manual_update.js
@@ -1,26 +1,20 @@
 'use strict'
+const fs = require('fs')
+const path = require('path')
 
-var fs = require('fs')
-var path = require('path')
-var _ = require('lodash')
-
-var codeRoot = __dirname
-var DEVICE_CONFIG_PATH = path.resolve(codeRoot, 'device_config.json')
-
-var deviceConfig = JSON.parse(fs.readFileSync(DEVICE_CONFIG_PATH))
-
-var config = deviceConfig.updater.extractor
-
+const codeRoot = path.resolve(__dirname)
+const deviceConfig = require(path.join(codeRoot, 'device_config.json'))
+const config = deviceConfig.updater.extractor
 config.skipVerify = true
-var extractor = require(codeRoot + '/lib/update/extractor').factory(config)
 
-var fileInfo = {
-  rootPath: '/opt/lamassu-updates/extract',
-  filePath: '/opt/lamassu-updates/download/update.tar'
-}
+const extractor = require(path.join(codeRoot, 'lib/update/extractor')).factory(config)
+
+const rootPath = '/opt/lamassu-updates/extract/'
+const downloadDirPath = '/opt/lamassu-updates/download/'
+const filePath = path.join(downloadDirPath, 'update.tar')
 
 function triggerWatchdog (cb) {
-  var donePath = '/opt/lamassu-updates/extract/done.txt'
+  const donePath = path.join(rootPath, 'done.txt')
   fs.writeFile(donePath, 'DONE\n', null, function (err) {
     if (err) throw err
     console.log('watchdog triggered')
@@ -28,11 +22,9 @@ function triggerWatchdog (cb) {
   })
 }
 
-process.on('SIGTERM', function () {
-  // Immune
-})
+process.on('SIGTERM', function () { /* Immune */ })
 
-extractor.extract(fileInfo, function (err) {
+extractor.extract({ rootPath, filePath, downloadDirPath }, function (err) {
   console.log('extracting...')
   if (err) throw err
   triggerWatchdog(function () { console.log('all done.') })
